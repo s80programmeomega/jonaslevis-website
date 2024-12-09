5d886e7861888eff17e30404fc537be7ced3641d
Jonas Levis <s80programmeomega@gmail.com>
2024-12-09 15:38:36 +0100

    Added the models

--- resume/apps.py
+++ resume/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ResumeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'resume'
+    
+    def ready(self) -> None:
+        from . import signals
